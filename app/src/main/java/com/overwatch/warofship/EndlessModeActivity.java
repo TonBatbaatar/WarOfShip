fbe3af868b755e2b6a03d08a35c89a6cfac009f9
SKYLIGHT <tvvn.mongol@qq.com>
2017-04-24 21:22:09 +0800

    Initial commit

---
+++ app/src/main/java/com/overwatch/warofship/EndlessModeActivity.java
@@ -0,0 +1,14 @@
+package com.overwatch.warofship;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndlessModeActivity extends AppCompatActivity {
+
+    //This is the fucking empty project of Overwatch group.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endless_mode);
+    }
+}
