e22e79c6bca5a0242d7b488f2f3d536f4aa64920
SKYLIGHT <tvvn.mongol@qq.com>
2017-04-24 21:39:49 +0800

    Copied the existing work.

--- app/src/main/java/com/overwatch/warofship/EndlessMode/EndlessModeActivity.java
+++ app/src/main/java/com/overwatch/warofship/EndlessMode/EndlessModeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class EndlessModeActivity extends AppCompatActivity {
 
+    //?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
