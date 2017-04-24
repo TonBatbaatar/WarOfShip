e7f5094a13670dd2fff0c793ebd1e4c9cdc382f6
SKYLIGHT <tvvn.mongol@qq.com>
2017-04-24 22:18:15 +0800

    Changed the directory fo the file.

---
+++ app/src/main/java/com/overwatch/warofship/GameImage/GameImageInterface.java
@@ -0,0 +1,9 @@
+package com.overwatch.warofship.GameImage;
+
+import android.graphics.Bitmap;
+
+public interface GameImageInterface {
+    public Bitmap getBitmap();
+    public float getX();
+    public float getY();
+}
