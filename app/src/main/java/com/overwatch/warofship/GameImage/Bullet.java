c3b56cf936600dbc92cbf3f00af3001643a12177
18604580811 <1320161461@qq.com>
2017-04-25 11:29:43 +0800

    jhfhj

---
+++ app/src/main/java/com/overwatch/warofship/GameImage/Bullet.java
@@ -0,0 +1,64 @@
+package com.overwatch.warofship.GameImage;
+
+/**
+ * Created by Administrator on 2017/4/25.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Bullet implements GameImageInterface {
+
+    private Bitmap myBulletImage;
+
+    private MyPlaneImage myPlaneImage;
+
+    private float x;
+    private float y;
+
+    public Bullet(Bitmap myBulletImage, MyPlaneImage myPlaneImage) {
+
+        this.myBulletImage = myBulletImage;
+        this.myPlaneImage= myPlaneImage;
+
+        x=myPlaneImage.getX()+myPlaneImage.getWidth()/2-10;
+        y=myPlaneImage.getY()+70;//-myBulletImage.getHeight();
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        y-=125;
+        return myBulletImage;
+    }
+
+    @Override
+    public float getX() {
+        return x;
+    }
+
+    @Override
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public boolean ifOutOfScreen(){
+        if(y<=-10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+}
+
