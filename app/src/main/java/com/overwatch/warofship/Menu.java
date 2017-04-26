0aaec1d0bbc337eea57587b91820f54909cbd12f
-Chris- <675189057@qq.com>
2017-04-26 21:09:31 +0800

    Welcome menu

---
+++ app/src/main/java/com/overwatch/warofship/Menu.java
@@ -0,0 +1,27 @@
+package com.overwatch.warofship;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.overwatch.warofship.EndlessMode.EndlessModeActivity;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(Menu.this,EndlessModeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
