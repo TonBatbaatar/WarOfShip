f38af4bc538e6bb07fe55b8bb330cfe22758908f
-Chris- <675189057@qq.com>
2017-04-26 22:23:45 +0800

    Annotation

--- app/src/main/java/com/overwatch/warofship/EndlessMode/EndlessModeGameView.java
+++ app/src/main/java/com/overwatch/warofship/EndlessMode/EndlessModeGameView.java
@@ -28,16 +28,17 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
     private GameLoop gameLoop;
     private SurfaceHolder holder=null;
 
-    private Paint p=new Paint();
+    private Paint p=new Paint();// Paint for all draw code.
 
-    private static int count;
-    private MyPlane selectedPlane;
+    private static int count;//controller of the speed of add a new item to the game.
+    private MyPlane selectedPlane;//used for control the ship.
 
+    //Class variable to store width and height fo the screen.
     public static int SCREEN_WIDTH;
     public static int SCREEN_HEIGHT;
 
 
-    //Create Bitmap for picture to store
+    //Create Bitmap for picture to store.
     private Bitmap backGround;
     private Bitmap myPlane;
     private Bitmap enemy;
@@ -47,43 +48,53 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
     private Bitmap boom;
     private Bitmap preparation;
 
+    //Class variable to store bullet images and game images.
+    //the reason for using class variable is for convenience.
+    //we need to use these tree variable in other classes.
     public static ArrayList<GameImageInterface> gameImages = new ArrayList();
     public static ArrayList<Bullet> bulletImages = new ArrayList();
     public static ArrayList<EnemyBullet> enemyBulletImages = new ArrayList();
 
+    //Constructor of the endless mode game view.
     public EndlessModeGameView(Context context){
 
         super(context);
-        gameLoop = new GameLoop(this);
-        this.setOnTouchListener(this);
+        gameLoop = new GameLoop(this);//initialize the new loop for the endless mode.
+        this.setOnTouchListener(this);//add the touch listener.
         holder=getHolder();
+        //Main part of run the game.
+        //Game start from here.
         holder.addCallback(
                 new SurfaceHolder.Callback(){
                     @Override
+                    //The thread begins from here
+                    //Game loop activated when the surface created.
                     public void surfaceCreated(SurfaceHolder holder) {
-                        gameLoop.setRunning(true);
-                        gameLoop.start();
+                        gameLoop.setRunning(true);//Set the run state to run
+                        gameLoop.start();//Start the thread
                     }
 
                     @Override
                     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                        EndlessModeGameView.SCREEN_WIDTH = width;
+                        EndlessModeGameView.SCREEN_WIDTH = width;//initialize the class variable when surface changed
                         EndlessModeGameView.SCREEN_HEIGHT = height;
-                        init();
+                        init();//initialize all of the pictures in the class
                     }
 
                     @Override
                     public void surfaceDestroyed(SurfaceHolder holder) {
-                        gameLoop.setRunning(false);
+                        gameLoop.setRunning(false);//set the run state of the game loop to stop
                     }
                 });
-        this.count=0;
+        this.count=0;//initialize the speed controller
     }
 
     ////Method for initialize the game images:
     //initialize the bitmaps with the images.
+    //this method is used in constructor
     private void init(){
 
+        //Preparation Bitmap is used for make the draw part more fluently
         preparation= Bitmap.createBitmap(SCREEN_WIDTH,SCREEN_HEIGHT, Bitmap.Config.ARGB_8888);
 
         backGround= BitmapFactory.decodeResource(getResources(), R.mipmap.background);
@@ -94,56 +105,64 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
         boom=BitmapFactory.decodeResource(getResources(),R.mipmap.boom);
         enemyBoss=BitmapFactory.decodeResource(getResources(),R.mipmap.enemyboss);
 
-        gameImages.add(new BackGround(backGround));
+        gameImages.add(new BackGround(backGround));//add bitmap to list
         gameImages.add(new MyPlane(myPlane,boom));
     }
 
     @Override
+    //the method of draw the bitmap to the screen
+    //this method is used in Game loop
     public void draw(Canvas canvas) {
 
         if(canvas!=null){
 
-            Canvas c=new Canvas(preparation);
-            count++;
+            Canvas c=new Canvas(preparation);//create a canvas to draw images and draw this preparation Bitmap at game loop
+            count++;//Speed controller to be updated
 
-            //// add enemy ship randomly
-            //if condition means:
-            //every five time add an enemy ship
-            //can change it to control the speed of add new enemy ship
+            //// add enemy ship randomly.
+            //if condition means that :
+            //every five time we run this method add an enemy ship
+            //can change it's value to control the speed of add new enemy ship
             if (count%5==0){
-                gameImages.add(new EnemyShip(enemy,boom));
+                gameImages.add(new EnemyShip(enemy,boom));//every five times we add an enemy ship
             }
-            if (count%50==0){
-                gameImages.add(new EnemyBossShip(enemyBoss,boom));
+            if (count%150==0){
+                gameImages.add(new EnemyBossShip(enemyBoss,boom));//every 150 times we add an enemy ship
             }
 
-            ////draw the picture to the screen except bullet
+            ////draw the picture to the screen except bullet.
+            //this for loop draw the pictures in game images list to canvas
             for (GameImageInterface image : (List<GameImageInterface>)gameImages.clone()){
 
+                //following draw method is for draw every bitmaps in the list
+                c.drawBitmap(image.getBitmap(),image.getX(),image.getY(),p);
+
+                //these two if conditions is for:
+                //shoot the bullet
+                //count is also speed contoller of bullet
+                if (image instanceof MyPlane && count%4==0){
+                    bulletImages.add(new Bullet(bullet,(MyPlane)image));
+                }
+                if (image instanceof EnemyBossShip && count%10==0){
+                    enemyBulletImages.add(new EnemyBullet(bullet,(EnemyBossShip)image));//shoot of enemy boss ship
+                }
+
+                //this if method is for:
+                //destroy the player ship when it crashed with enemy ships
                 if (image instanceof MyPlane){
                     ((MyPlane) image).isBeat(gameImages,bulletImages);
                 }
 
+                //these two if conditions is for :
+                //destroy the enemy ships when it destroyed
                 if (image instanceof EnemyShip){
                     ((EnemyShip) image).isBeat(bulletImages);
-                    Log.i("REMOVE","Destroied the enemy ship!");
+                    Log.i("TEST.REMOVE","Enemy ship is destroyed!");
                 }
-
                 if (image instanceof EnemyBossShip){
                     ((EnemyBossShip) image).isBeat(bulletImages);
-                    Log.i("REMOVE","Destroied the enemy boss ship!");
+                    Log.i("TEST.REMOVE","Destroyed the enemy boss ship!");
                 }
-
-                c.drawBitmap(image.getBitmap(),image.getX(),image.getY(),p);
-
-                if (image instanceof MyPlane && count%4==0){
-                    bulletImages.add(new Bullet(bullet,(MyPlane)image));
-                }
-
-                if (image instanceof EnemyBossShip && count%10==0){
-                    enemyBulletImages.add(new EnemyBullet(bullet,(EnemyBossShip)image));
-                }
-
             }
 
             ////draw the bullet image to the screen
@@ -163,19 +182,19 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
                 break;
             }
 
+            //following draw method is draw the preparation Bitmap to screen.
             canvas.drawBitmap(preparation,0,0,p);
         }
     }
 
     @Override
+    //Following method is code for touch listener
     public boolean onTouch(View v, MotionEvent event) {
 
         if (event.getAction()==MotionEvent.ACTION_DOWN){
-
             for (GameImageInterface image: gameImages){
-
                 if (image instanceof MyPlane){
-
+                    //this if method is select the ship when we touch on it
                     if (((MyPlane) image).ifPlaneSelected(event.getX(),event.getY())){
                         selectedPlane=(MyPlane)image;
                     }else {
@@ -184,9 +203,9 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
                     break;
                 }
             }
-
         } else if (event.getAction()==MotionEvent.ACTION_MOVE){
 
+            //this if method is actual part of draw at location we moved
             if (selectedPlane!=null){
                 float newX=event.getX()-(selectedPlane.getWidth()/2);
                 float newY=event.getY()-(selectedPlane.getHeight()/2);
@@ -195,14 +214,8 @@ public class EndlessModeGameView extends SurfaceView implements View.OnTouchList
             }
 
         } else if (event.getAction()==MotionEvent.ACTION_UP){
-
-            selectedPlane=null;
-
+            selectedPlane=null;//release the ship
         }
-
         return true;
     }
-
-
-
 }
