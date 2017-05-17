393aee5140e809044eeb25b09af72ec777d8ae75
-Chris- <675189057@qq.com>
2017-05-17 23:10:10 +0800

    5 storys

---
+++ app/src/main/java/com/overwatch/warofship/StoryActivity.java
@@ -0,0 +1,66 @@
+package com.overwatch.warofship;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.overwatch.warofship.EndlessMode.Story1LevActivity;
+import com.overwatch.warofship.EndlessMode.Story2LevActivity;
+import com.overwatch.warofship.EndlessMode.Story3LevActivity;
+import com.overwatch.warofship.EndlessMode.Story4LevActivity;
+import com.overwatch.warofship.EndlessMode.Story5LevActivity;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.story_layout);
+        Button button1=(Button) findViewById(R.id.button_6);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(StoryActivity.this,Story1LevActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button2=(Button) findViewById(R.id.button_7);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(StoryActivity.this,Story2LevActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button3=(Button) findViewById(R.id.button_8);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(StoryActivity.this,Story3LevActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button4=(Button) findViewById(R.id.button_9);
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(StoryActivity.this,Story4LevActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button5=(Button) findViewById(R.id.button_10);
+        button5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent=new Intent(StoryActivity.this,Story5LevActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
